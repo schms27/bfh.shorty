81a36c2e88453878d8f1f72f6bf76435fe22b7a5
simon.schmid <schms27@gmail.com>
2018-01-18 16:53:16 +0100

    renaming of repos

--- short-link-service/src/main/java/bfh/shorty/targetservice/repositories/ShortLinkRepository.java
+++ short-link-service/src/main/java/bfh/shorty/targetservice/repositories/ShortLinkRepository.java
@@ -1,9 +1,11 @@
 package bfh.shorty.targetservice.repositories;
 
+import bfh.shorty.targetservice.entities.ShortLink;
 import bfh.shorty.targetservice.entities.Target;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface TargetRepository extends CrudRepository<Target, String> {
+public interface ShortLinkRepository extends CrudRepository<ShortLink, String> {
+
 }
