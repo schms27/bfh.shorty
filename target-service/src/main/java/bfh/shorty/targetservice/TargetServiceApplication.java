30199cfd80a3883688bb07862c902d188f072199
Simon Schmid <schms27@gmail.com>
2018-01-24 13:21:45 +0100

    added target-service

---
+++ target-service/src/main/java/bfh/shorty/targetservice/TargetServiceApplication.java
@@ -0,0 +1,12 @@
+package bfh.shorty.targetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetServiceApplication.class, args);
+	}
+}
