c2515e7fd7e9c0a7907701f19697465a577c6149
Adrian Aulbach <adrian@aulbach.ch>
2018-01-31 23:17:33 +0100

    Add frontend-service to Hystrix-Dashboard

--- frontend-service/src/main/java/bfh/shorty/frontendservice/FrontendServiceApplication.java
+++ frontend-service/src/main/java/bfh/shorty/frontendservice/FrontendServiceApplication.java
@@ -3,11 +3,15 @@ package bfh.shorty.frontendservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableHystrixDashboard
+@EnableTurbine
 public class FrontendServiceApplication {
 
 	public static void main(String[] args) {
